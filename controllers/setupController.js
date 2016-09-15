6180f53c3fbe33415bc53006e52a5461200ceab9
nickhalden <nipun.chawla786@gmail.com>
2016-09-14 20:12:28 -0500

    added more feed data

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -4,25 +4,6 @@
 var Model = require('../models/todoModel');
 
 
-// module.exports = function(app) {
-//
-//    app.get('/create', function(req, res) {
-//
-//        // seed database
-//        var results = [
-//         {
-//             'username' : 'nipun',
-//             'todo' : 'nothing'
-//         }
-//        ];
-//
-//        Model.create(starterModel, function(err, results) {
-//            res.send(error);
-//        });
-//        res.send('bottom');
-//    });
-//
-// }
 module.exports = function(app) {
 
    app.get('/create', function(req, res) {
@@ -31,7 +12,15 @@ module.exports = function(app) {
        var results = [
                {
                    'username' : 'nipun',
-                   'todo' : 'nothing'
+                   'todo' : 'Learn Nodejs'
+               },
+               {
+                   'username' : 'nipun',
+                   'todo' : 'Buy Milk'
+               },
+               {
+                   'username' : 'nipun',
+                   'todo' : 'Read Novels'
                }
         ];
        Model.create(results, function(err, results) {
--- models/todoModel.js
+++ models/todoModel.js
@@ -12,19 +12,3 @@ var todoSchema = new mongoose.Schema({
 var model = mongoose.model('data',todoSchema);
 
 module.exports = model;
-//'mongodb://127.0.0.1:27017/checkout/data
-
-
-// var Schema = new mongoose.Schema({
-//  'orderId'                 : String,
-//  'buyerInfo'               : JSON,
-//  'purchaseContractLocation': JSON,
-//  'version'                 : String,
-//  'purchaseContractItems'   : JSON,
-//  'createdDate'             : String
-// },{ collection: 'data' });
-//
-//
-// var model = mongoose.model('data', Schema);
-//
-// module.exports = model;
